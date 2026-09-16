e6bb98131cdfe893a0a99487a258deda862d754f
Sumit Dhiman <143sumit05@gmail.com>
2026-09-16 11:57:26 +0530

    Playing with routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use(("/profile"),(req,res)=>{
+
+
+app.get("/profile",(req,res)=>{
     res.send("PROFILE KHULGYI");
 })
-
-app.use(("/login"),(req,res)=>{
-    res.send("LOGIN PAGE KHULGYA");
+app.post(("/profile"),(req,res)=>{
+    res.send("SuccesFully Posted data");
 })
+// app.use(("/login"),(req,res)=>{
+//     res.send("LOGIN PAGE KHULGYA");
+// })
 
-app.use(("/"),(req,res)=>{
-    res.send("NODEMON SE CHALAYA SERVER");
-})
+// app.use(("/"),(req,res)=>{
+//     res.send("NODEMON SE CHALAYA SERVER");
+// })
 
 app.listen(3000,()=>{
     console.log("SERVER STARTED AT 3000");
