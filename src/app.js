7e7a9c5d6a2433474c6114e7a4bbc9839684f4a5
Sumit Dhiman <143sumit05@gmail.com>
2026-09-18 01:51:07 +0530

    Route Handlers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 
 const app = express();
-
-
-
-app.get("/profile",(req,res)=>{
-    res.send("PROFILE KHULGYI");
-})
-app.post(("/profile"),(req,res)=>{
-    res.send("SuccesFully Posted data");
-})
+// app.get("/profile",(req,res)=>{
+//     res.send("PROFILE KHULGYI");
+// })
+// app.post(("/profile"),(req,res)=>{
+//     res.send("SuccesFully Posted data");
+// })
 // app.use(("/login"),(req,res)=>{
 //     res.send("LOGIN PAGE KHULGYA");
 // })
@@ -17,6 +14,23 @@ app.post(("/profile"),(req,res)=>{
 // app.use(("/"),(req,res)=>{
 //     res.send("NODEMON SE CHALAYA SERVER");
 // })
+
+app.use("/user",(req,res,next)=>{
+    console.log("1");
+    next();
+    // res.end("RES1");
+ 
+},[(req,res,next)=>{
+    console.log("2");
+    next();
+    
+}],(req,res,next)=>{
+    console.log(3);
+    res.end("3");
+})
+
+
+
 
 app.listen(3000,()=>{
     console.log("SERVER STARTED AT 3000");
