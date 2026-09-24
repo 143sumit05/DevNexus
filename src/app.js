8dfb6ca78da61e278c4d18e33f8642064d5af7ab
Sumit Dhiman <143sumit05@gmail.com>
2026-09-24 15:00:25 +0530

    deepintoAPI's

--- src/app.js
+++ src/app.js
@@ -12,20 +12,41 @@ connectDB().then(()=>{
     console.log("SERVER STARTED AT 3000");
 })
 }).catch((err)=>{
+    console.log(err);
     console.log("Cant connect to Database");
+  
 })
-app.post("/signup",async (req,res)=>{
-    const user = new User({
-        firstName : "Shivani",
-        lastName : "Modi",
-        emailId : "143shivani05@gmail.com",
-        password : "shivani123",
-        age : 20,
-        gender : "female"
-    })
-    await user.save();
-    res.send("Saved to Database");
+app.use(express.json())
+
+// app.get("/userEmail",async(req,res)=>{
+//     const users = req.body.emailId;
+//     try{
+//         const user = await User.find({})
+//         res.send(user);
+//     }
+//     catch(err){
+//         res.status(400).send("Something went wrong");
+//     }
+// })
+app.get("/feed",async(req,res)=>{
+    const users = req.body.emailId;
+    try{
+        const user = await User.find({})
+        res.send(user);
+    }
+    catch(err){
+        res.status(400).send("Something went wrong");
+    }
 })
+
+
+
+// app.post("/signup",async (req,res)=>{
+//     const user = new User(req.body);
+//     console.log(req.body)
+//     await user.save();
+//     res.send("Saved to Database");
+// })
 app.use("/",(req,res)=>{
     res.end("HEHE SERVER CHALPEYA");
 })
